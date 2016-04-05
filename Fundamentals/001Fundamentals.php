4e58477d051d564c210799faeb2b77c418db7ae4
Jonathan Ben-Ammi <jbenammi@gmail.com>
2016-04-05 11:39:35 -0700

    Made comments to each function explaining what the function is doing.

--- Fundamentals/001Fundamentals.php
+++ Fundamentals/001Fundamentals.php
@@ -1,4 +1,5 @@
 <?php
+// This Function will run when called and will go through a for "loop" iteration, with a starting variable $i that has been set at 100. It will echo the variable and then increment $i by two after each echo. This will continue until $i is no longer less than the set value of 1000.
 function oddNumbers()
 {
   for ($i = 100; $i < 1000; $i += 2)
@@ -6,8 +7,9 @@ function oddNumbers()
       echo($i);
   }
 }
-// oddNumbers();
+oddNumbers();
 
+//This function will run when called and will go through a for "loop" iteration, with a starting variable $i that has been set at 0. It will echo the variable and then increment $i by five after each echo. This will continue until $i is no longer less than one million. This will result in printing all the multiple numbers of 5 through one million as a string.
 function multiplesOfFive()
 {
   for ($i = 0; $i <= 1000000; $i +=5)
@@ -15,6 +17,11 @@ function multiplesOfFive()
     echo $i;
   }
 }
+//Moved Call to function
+multiplesOfFive();
+
+//This function will run when called and will pass through an argument set with a parameter variable called $arr. As we are looking for the sum of all values in the array, we will declare a variable named $sum that we will set initially to zero. We will then go through a for "loop" iteration, with a starting variable $i that has been set to zero. it will take the value of the variable $sum and add to it the value from the spot in the array being evaluated noting the spot in the array by the value of variable $i. It will then increment $i by one. It will continue to do this until the value of $i is no longer less than the value of of the length of the $arr variable being passed through. The function will then echo the value of the variable $sum as a string and return the value of the variable $sum.
+
 $array = [1,2,3,4,6];
 
 function sumOfArrayValues($arr){
@@ -26,10 +33,12 @@ function sumOfArrayValues($arr){
   echo $sum;
   return $sum;
 }
-// sumOfArrayValues($array);
-// multiplesOfFive();
+sumOfArrayValues($array);
 // Alt strategy
 
+//This function will run when called and will pass through an argument set with a parameter variable called $arr. As we are looking for the sum of all the values in the array, we will declare a variable named $sum that we will set initially to zero. We will then use a foreach "loop" iteration, that will start at the beginning of the array. It will take the value of the variable $sum and add to it the value at that spot in the array. It will then go through the next spot in the array and continue until it has run the last point in the array. The function will then echo the value of the variable $sum as a string and return the value of the variable $sum.
+
+//Added an echo to test function
 function sumOfArrayValues2($arr)
 {
   $sum = 0;
@@ -37,14 +46,22 @@ function sumOfArrayValues2($arr)
   {
     $sum += $value;
   }
+  echo $sum;
   return $sum;
 }
+
+sumOfArrayValues2($array);
+
+
+//This function will work in tandem with the function above and will when called pass through an argument (in this case $array) and set with a parameter variable called $arr. It will then echo the call back of the function sumOfArrayValues2($arr) divided by the count length of the $arr being passed through. This should result in the average number for the $array initially called with the function.
 
 function average($arr)
 {
   echo (sumOfArrayValues2($arr)/count($arr));
 }
-// average($array);
+average($array);
+
+//This function when called will set a new empty array called $arr. It will then use a for "loop" iteration with a starting variable $i set to one. it will push the value of $i to the empty array and then increment $i by 2. This will continue until $i is no longer less than the set number of twenty thousand. It will then fuse function var_dump to print out the new array.
 
 function buildOddArray()
 {
@@ -54,7 +71,10 @@ function buildOddArray()
   }
   var_dump($arr);
 }
-// buildOddArray();
+buildOddArray();
+
+//This function when called will initiate a for "loop" iteration where the initial variable $i has been set to one. if variable $i divided by two will not have a remainder as noted by equaling zero which makes the statement "true" it will echo a string the value of $i is even. If this statement is 'false' it will echo a string the value of $i is odd. $i will then increment by one and will run the check again until $i is is no longer less than the set amount of 2000.
+
 function oddOrEven()
 {
   for ($i=1; $i < 2000; $i++) {
@@ -68,13 +88,17 @@ function oddOrEven()
     }
   }
 }
-// oddOrEven();
+oddOrEven();
+
+
+//This function when called will pass through two arguments to two parameters ($arr and $val). The function will initiate a for "loop" iteration where the initial variable $i has been set to zero. the iteration will set the variable  passed through and the position as stated by variable $i and set it to the it's self times the $val that was passed through the function. the for "loop" will then increment $i by one. It will continue this set of functions until $i is no longer less than the length of the array passed through the function. It will then return the new value of $arr.
+
+
 
 function multiply($arr,$val)
 {
   for ($i=0; $i < count($arr); $i++) {
     $arr[$i] =   $arr[$i]*$val;
-    # code...
   }
   return $arr;
 }
